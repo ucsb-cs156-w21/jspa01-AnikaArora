c150b47c57da2880643bb13ac45cc5b4f5138189
Anika Arora <anika_arora@umail.ucsb.edu>
2021-01-19 20:18:52 -0800

    resolved all test cases except for toString()

--- javascript/src/main/rationals/Rational.js
+++ javascript/src/main/rationals/Rational.js
@@ -10,7 +10,17 @@ export default class Rational {
 
     this.numerator = Math.trunc(numerator);
     this.denominator = Math.trunc(denominator);
-
+    if (this.denominator === 0) {
+	throw new Error("error message goes here");
+    }
+    if (this.numerator < 0 && this.denominator < 0) {
+	this.numerator *= -1;
+	this.denominator *= -1;
+    }
+    if (this.numerator > 0 && this.denominator < 0) {
+        this.numerator *= -1;
+        this.denominator *= -1;
+    }
     if (this.numerator !== 0) {
       const greatestCommonDivisor = gcd(this.numerator, this.denominator);
       this.numerator /= greatestCommonDivisor;
@@ -18,9 +28,13 @@ export default class Rational {
     }
   }
 
-  toString() {
-    return "";
-  }
+    toString() {
+    const numerator = this.numerator;
+    const denominator = this.denominator; 
+    var num = numerator.toString();
+    var den = denominator.toString();
+	var result = num + '/' + den; 
+    }
 
   plus(other) {
     Rational.verifyIsRational(other);
@@ -48,31 +62,66 @@ export default class Rational {
   }
 
   times(other) {
-    return 42;
+      if (this.numerator === 0 || other.numerator === 0 || this.denominator === 0 || other.denominator === 0) {
+	  return new Rational(0,1); 
+      }
+      const numerator = (this.numerator * other.numerator);
+      const denominator = (this.denominator * other.denominator);
+      
+      return new Rational(numerator,denominator); 
   }
 
   static multiply(first, second) {
-    return 42;
+      //if (first === 0 || second === 0) {
+         //return new Rational(0,1); 
+      //}
+      Rational.verifyIsRational(first);
+      Rational.verifyIsRational(second);
+      return first.times(second); 
   }
 
   minus(other) {
-    return 42;
+      if (this.denominator === other.denominator) {
+	  const numerator = (this.numerator * other.numerator);
+	  return new Rational(numerator,this.denominator); 
+      }
+      else if (this.denominator > other.denominator) {
+	  const denominator = this.denominator;
+	  const numerator = (this.numerator-(other.numerator*this.denominator));
+	  return new Rational(numerator,denominator); 
+      }
+      else {
+	  const denominator = other.denominator;
+	  const numerator = ((this.numerator*other.denominator)-other.numerator);
+	  return new Rational(numerator,denominator); 
+      }
   }
 
   static subtract(first, second) {
-    return 42;
+      Rational.verifyIsRational(first);
+      Rational.verifyIsRational(second);
+      return first.minus(second); 
   }
 
   reciprocal() {
-    return 42;
+      if (this === 0) {
+	  throw new Error("error message");
+      }
+      else {
+	  return new Rational(this.denominator,this.numerator); 
+      }
   }
 
   dividedBy(other) {
-    return 42;
+      const numerator = this.numerator*other.denominator;
+      const denominator = this.denominator*other.numerator;
+      return new Rational(numerator,denominator); 
   }
 
   static quotient(first, second) {
-    return 42;
+      Rational.verifyIsRational(first);
+      Rational.verifyIsRational(second);
+      return first.dividedBy(second);
   }
 
   static verifyIsRational(object) {
